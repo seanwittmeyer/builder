e12c865ac2b3c04ae46020fa5f1bd7e927139638
seanwittmeyer <seanwittmeyer@users.noreply.github.com>
2020-05-22 18:15:51 +0000

    cleaned up the head

--- application/views/app/builder/head.php
+++ application/views/app/builder/head.php
@@ -1,5 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 	if (!isset($loadjs)) $loadjs = array();
+	$uniquejsversion = time(); // Set this to the date last edited or time() so that client js and css refreshes the cache
 	?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,22 +17,13 @@
 	<link href="/includes/css/summernote.css" rel="stylesheet" />
 	<link href="/includes/css/font-awesome.min.css" rel="stylesheet">
 	<?php if (in_array('nvd3', $loadjs)) { ?> 
-	<link href='/includes/css/nvd3.css?20181203' rel='stylesheet' />
+	<link href='/includes/css/nvd3.css' rel='stylesheet' />
 	<?php } ?> 
-	<link href="/includes/css/builder.css?20200519" rel="stylesheet">
-	<script src="/dist/index.bundle.js?<?=time()?>"></script>
-	<!-- - ->
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-	-->
-	<script>window.jQuery || document.write('<script src="/includes/js/jquery.min.js"><\/script>')</script>
-	<script src="/dist/general.bundle.js?<?=time()?>"></script>
-	<?php if (isset($loadjs['builder_viz'])) { ?> 
-	<script src="/dist/viz.bundle.js?<?=time()?>"></script>
-	<?php } ?>
-	<?php if (isset($loadjs['builder_maps'])) { ?> 
-	<script src="/dist/maps.bundle.js?<?=time()?>"></script>
+	<link href="/includes/css/builder.css?<?=$uniquejsversion?>" rel="stylesheet">
+	<script src="/dist/index.bundle.js?<?=$uniquejsversion?>"></script>
+	<script src="/dist/general.bundle.js?<?=$uniquejsversion?>"></script>
+	<?php if (isset($loadjs['builder_viz'])) { ?><script src="/dist/viz.bundle.js?<?=$uniquejsversion?>"></script><?php } ?> 
+	<?php if (isset($loadjs['builder_maps'])) { ?><script src="/dist/maps.bundle.js?<?=$uniquejsversion?>"></script>
 	<script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.45.0/mapbox-gl.js'></script>
 	<link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.45.0/mapbox-gl.css' rel='stylesheet' />
 	<?php } ?> 
